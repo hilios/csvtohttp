8ea43b91f906a7c371abfefad9d1eca4ac2bcc96
Edson <edson.hilios@gmail.com>
2024-04-30 12:59:43 +0200

    Add request log message for non verbose run

--- csvtohttp/request.py
+++ csvtohttp/request.py
@@ -8,26 +8,30 @@ logger = logging.getLogger(__name__)
 
 
 async def send_request(session, method, url, headers, payload, dry_run=True):
-    logger.debug(textwrap.dedent(f"""
-      Sending request:
-      {method} {url} HTTP/1.1
-      {'\n      '.join([f"{k}: {v}" for (k, v) in headers.items()])}
-      \t\t
-    """).lstrip() + payload)
+    request_id = uuid.uuid4()
+
+    if logger.level == logging.DEBUG:
+        logger.debug(textwrap.dedent(f"""
+        Sending request:
+        {method} {url} HTTP/1.1
+        {'\n      '.join([f"{k}: {v}" for (k, v) in headers.items()])}
+        \t\t
+        """).lstrip() + payload)
+    else:
+        logger.info(f"[{request_id}] Request to {method} {url}", extra={'request': payload})
 
     if dry_run:
         return None
 
-    index = uuid.uuid4()
     start_time = time.time()
     async with session.request(method, url, headers=headers, data=payload) as response:
         text = await response.text()
     elapsed_time = time.time() - start_time
 
     if logger.level == logging.DEBUG:
-        logger.debug(f"[{index}] Response to took {elapsed_time:.2f} seconds\n{response}")
+        logger.debug(f"[{request_id}] Response to took {elapsed_time:.2f} seconds\n{response}")
     else:
-        logger.info(f"[{index}] Response to took {elapsed_time:.2f} seconds", extra={'response': response})
+        logger.info(f"[{request_id}] Response to took {elapsed_time:.2f} seconds", extra={'response': response})
 
     if response.status != 200:
         logger.error(f"HTTP call failed with status {response.status}\n{text}", extra={'response': text})
